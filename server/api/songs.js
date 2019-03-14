cad0410d425474908266414e1999b009aecf4140
Emmy Panken <erpanken@gmail.com>
2019-03-14 14:43:04 -0400

    feature (node backend route) we can now get all songs from our databse

--- server/api/songs.js
+++ server/api/songs.js
@@ -1,1 +1,16 @@
 //get one song
+const router = require('express').Router();
+const Song = require('../db/models/Songs');
+
+//get song by ID
+//api/songs
+router.get('/', async (req, res, next) => {
+  try {
+    const songList = await Song.findAll();
+    res.status(200).send(songList);
+  } catch (error) {
+    next(error);
+  }
+});
+
+module.exports = router;
