822c72cb2971be6c64a05ab7be7e7987ce0396fd
zacharyAD <zacharyadcoding@gmail.com>
2019-03-15 16:28:48 -0400

    added a non-working redux cycle for clicking a button to check an api from spotify

--- app/components/Playlist.js
+++ app/components/Playlist.js
@@ -1,20 +1,41 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { fetchPlaylist } from '../reducers/playlistStore';
+import {
+  fetchPlaylist,
+  CheckFetchSpotify,
+  moveTopVoteToDeck,
+} from '../reducers/playlistStore';
 import SongCard from './SongCard';
 
 export class Playlist extends React.Component {
+  constructor(props) {
+    super(props);
+    // this.state = {
+    //   songList: [],
+    // };
+    this.CheckSpotify = this.CheckSpotify.bind(this);
+  }
   componentDidMount() {
     this.props.fetchedPlaylist();
+  }
+
+  CheckSpotify() {
+    this.props.isSongDone();
+    this.props.moveToDeck();
   }
 
   render() {
     return (
       <div>
+        <button type={'button'} onClick={() => CheckSpotify()}>
+          Check Spotify
+        </button>
         <h1>test</h1>
-        {this.props.playlist.map(song => {
-          return <SongCard key={song.songSpotifyId} song={song} />;
-        })}
+        <div>
+          {this.props.playlist.map(song => {
+            return <SongCard key={song.songSpotifyId} song={song} />;
+          })}
+        </div>
       </div>
     );
   }
@@ -26,6 +47,8 @@ const mapStateToProps = state => ({
 
 const mapDispatchToProps = dispatch => ({
   fetchedPlaylist: () => dispatch(fetchPlaylist()),
+  isSongDone: () => dispatch(CheckFetchSpotify()),
+  moveToDeck: () => dispatch(moveTopVoteToDeck()),
 });
 
 export default connect(
--- app/reducers/playlistStore.js
+++ app/reducers/playlistStore.js
@@ -3,15 +3,29 @@
 // import loggingMiddleware from 'redux-logger';
 // import thunkMiddleware from 'redux-thunk';
 import axios from 'axios';
+import SongCard from '../components/SongCard';
 
+//STATE AND REDUCER
+const initialState = {
+  songs: [],
+  currSong: {},
+  deckSong: {},
+};
 //ACTION TYPES
 const GET_SONGS = 'GET_SONGS';
-
+const GOT_NEXT = 'GOT_NEXT';
 //ACTION CREATORS
 const getSongs = playlist => {
   return {
     type: GET_SONGS,
     playlist,
+  };
+};
+
+const gotNext = spotifyPlaying => {
+  return {
+    type: GOT_NEXT,
+    spotifyPlaying,
   };
 };
 
@@ -28,10 +42,26 @@ export const fetchPlaylist = () => {
   };
 };
 
-//STATE AND REDUCER
-const initialState = {
-  songs: [],
-  currSong: {},
+export const CheckFetchSpotify = () => {
+  return async dispatch => {
+    try {
+      const dataFromSpotify = await axios.get(`/api/spotify/`);
+      //The next/on deck song is playing
+      if (dataFromSpotify.id !== initialState.currSong.id) {
+        const postedSongToSpotify = await axios.post('', {
+          songId: initialState.deckSong,
+        });
+        const action = gotNext(dataFromSpotify);
+        dispatch(action);
+      } else {
+        //Same song is still playing
+        //hits reducer's defalt may not be needed but this will just keep the same state.
+        dispatch({ type: 'SAME_SONG_PLAYING' });
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
 };
 
 const playlistReducer = (state = initialState, action) => {
@@ -43,9 +73,15 @@ const playlistReducer = (state = initialState, action) => {
         currSong: state.songs[0],
       };
     }
+    case GOT_NEXT: {
+      return {
+        ...state,
+        currSong: action.spotifyPlaying,
+      };
+    }
     default: {
       return state;
     }
   }
 };
-export default playlistReducer
+export default playlistReducer;
